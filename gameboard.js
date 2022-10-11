13c5cf372221b5d84701379f9a0419be6159171a
MidStein <deepakchauhan19feb@gmail.com>
2022-10-11 18:29:11 +0530

    Gameboard module

---
+++ gameboard.js
@@ -0,0 +1,24 @@
+export default function Gameboard() {
+    const missed = [];
+
+    function recieveAttack(coordinates) {
+        for (let i = 0; i < 10; i += 1) {
+            if (shipAreas[i].includes(coordinates)) {
+                ship[i].hit();
+                return;
+            }
+            missedCoord.push(coordinates);
+        }
+    }
+    function allSunk() {
+        for (let i = 0; i < 10; i += 1) {
+            if (!ship[i].isSunk()) return false;
+        }
+        return true;
+    }
+
+    return {
+        recieveAttack,
+        allSunk
+    };
+};
---
+++ gameboard.test.js
@@ -0,0 +1,24 @@
+import Gameboard from "./gameboard";
+
+let instance;
+beforeEach(() => {
+    instance = Gameboard();
+});
+
+test('recieveAttack() gets hits on ships', () => {
+    instance.recieveAttack([5, 0]);
+    expect(instance.ship[0].getHitsCount()).toBe(1);
+});
+test('recieveAttack() tracks missed attacks', () => {
+    instance.recieveAttack([0, 0]);
+    expect(instance.missed).toContain([0, 0]);
+});
+
+test('allSunk() works', () => {
+    instance.recieveAttack([2, 0]);
+    instance.recieveAttack([3, 0]);
+    instance.recieveAttack([4, 0]);
+    instance.recieveAttack([5, 0]);
+    instance.recieveAttack([6, 0]);
+    expect(instance.allSunk()).toBeFalsy();
+})
